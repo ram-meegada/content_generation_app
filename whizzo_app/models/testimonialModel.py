1ab1e3f5ca24599b65efb2161bf8c5e9f2da094c
ram-meegada <ramumeegada1811@gmail.com>
2024-05-16 16:35:18 +0530

    changes

--- whizzo_app/models/testimonialModel.py
+++ whizzo_app/models/testimonialModel.py
@@ -7,6 +7,7 @@ class TestimonialModel(BaseModel):
     profile_picture = models.ForeignKey(UploadMediaModel, on_delete=models.SET_NULL, null=True)
     first_name = models.CharField(max_length=255, default="")
     last_name = models.CharField(max_length=255, default="")
+    country_code = models.CharField(max_length=10,default="")
     email = models.CharField(max_length=255, default="")
     phone_no = models.CharField(max_length=100, default="")
     rating = models.FloatField(default=0)
