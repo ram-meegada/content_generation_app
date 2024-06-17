d76f7e3fc30f3842452b362eebdd11f08e62ff4c
ram-meegada <ramumeegada1811@gmail.com>
2024-06-17 12:12:42 +0530

    prompt

--- whizzo_app/services/categoryService.py
+++ whizzo_app/services/categoryService.py
@@ -1197,7 +1197,7 @@ class CategoryService:
                     ## old ###
                     # "text": "I am an invligator i will give you scaned pdf of images. Whatever you find text in images give question and answer regarding them.And dont give random answers"},
                     ### new ####
-                    "text": "You are a teacher. Generate questions and answers based on the data I provide to you. Format should be proper Python Javascript object notation list of dictionaries where every dictionary contains keys as 'question', 'answer' and 'options'(if available)."},
+                    "text": "You are a teacher. Generate questions and answers based on the data I provide to you. Format should be proper Python Javascript object notation list of dictionaries where every dictionary contains keys as 'question', 'correct_answer' and 'options'(if available)."},
                     # "text": "I am an invligator to mark the questions i need correct answers ,provide me correct answers for these questions and when needed diagrams and figures or explanations just give concise answers and give answers to remaining questions,lastly provide the answers in json list format (question no. ,question, options(this field will will only be there if options are present else no need ), correct answer)"},
                 {"type": "text", "text":text_data}
             ]
