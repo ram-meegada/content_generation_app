92ad7b9209fb95b770e233c85236b9db85e0fe73
ram-meegada <ramumeegada1811@gmail.com>
2024-07-12 15:49:44 +0530

    changes

--- whizzo_app/models/abilityModel.py
+++ whizzo_app/models/abilityModel.py
@@ -4,7 +4,7 @@ from django.contrib.postgres.fields import ArrayField
 class AbilityModel(models.Model):
     question = models.CharField(max_length=255, default="")
     answer_option=ArrayField(models.CharField(max_length=255,default=""), blank=True, null=True)
-    corect_answer = models.CharField(max_length=255, default="")
+    corect_answer = models.CharField(max_length=1000, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
